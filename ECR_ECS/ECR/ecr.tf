c7bfc2c4b02ccf3a40dddc6d105adf9f39ad41c9
oscardasilva <oscar.silva@ritain.io>
2023-05-04 09:41:50 +0100

    Build Docker Image

--- ECR_ECS/ECR/ecr.tf
+++ ECR_ECS/ECR/ecr.tf
@@ -7,3 +7,14 @@ provider "aws" {
 resource "aws_ecr_repository" "my_repository" {
   name = "my-repository"
 }
+
+################################
+#           Outputs            #
+################################
+output "aws_ecr_registry_id" {
+  value = aws_ecr_repository.my_repository.registry_id
+}
+
+output "aws_ecr_repository_url" {
+  value = aws_ecr_repository.my_repository.repository_url
+}
