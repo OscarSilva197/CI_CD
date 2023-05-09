7491ccaacc6302330ec4a732e1237027296a2967
oscardasilva <oscar.silva@ritain.io>
2023-05-09 12:02:58 +0100

    ecr

--- ECR_ECS/INFRA/ecs.tf
+++ ECR_ECS/INFRA/ecs.tf
@@ -127,7 +127,7 @@ resource "aws_ecs_task_definition" "dummy_api_task" {
   [
     {
       "name"      : "base_api",
-      "image"     : "${var.docker_image_name}",
+      "image"     : "tmf632-party-mgmt-api",
       "cpu"       : 512,
       "memory"    : 1024,
       "essential" : true,
