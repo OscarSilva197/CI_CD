3e84fe3aca1378f2e77d417fa6c9b5cd332a3e3d
oscardasilva <oscar.silva@ritain.io>
2023-05-09 12:18:20 +0100

    ecr

--- ECR_ECS/ECR/ecr.tf
+++ ECR_ECS/ECR/ecr.tf
@@ -7,12 +7,14 @@ provider "aws" {
 # Cria um repositório ECR
 resource "aws_ecr_repository" "my_repository" {
   name = "my-repository"
-  image_tag_mutability            = "IMMUTABLE"             # Added after checkov analysis
-  image_scanning_configuration {                            # Added after checkov analysis
-    scan_on_push = true
-  } 
-  encryption_configuration {                                # Added after checkov analysis
-    encryption_type                 = "KMS"                   
-  }
-  force_delete                    = true
+}
+
+#           Outputs            #
+
+output "aws_ecr_registry_id" {
+  value = aws_ecr_repository.my_repository.registry_id
+}
+
+output "aws_ecr_repository_url" {
+  value = aws_ecr_repository.my_repository.repository_url
 }
