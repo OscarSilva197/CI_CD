29fbcae8482a16d1cf4cd2044ca5919802f25292
oscardasilva <oscar.silva@ritain.io>
2023-05-10 14:35:42 +0100

    teste

--- ECR_ECS/INFRA/main.tf
+++ ECR_ECS/INFRA/main.tf
@@ -16,7 +16,6 @@ terraform {
 terraform {
   required_version = ">= 1.0.7"
   required_providers {
-    
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 2.20.0"
@@ -33,12 +32,14 @@ terraform {
   }
 }
 
+provider "aws" {
+  access_key = "AKIA3GVPGHW4VN46WPWN"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+
 
 
 provider "docker" {}
 
-/*
-provider "aws" {
-  region     = "us-east-1"
-}
-*/
+
