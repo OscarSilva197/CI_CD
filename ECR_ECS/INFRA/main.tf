c7be60cce614fffb9d62c786cd3ab41f9a9dfde6
oscardasilva <oscar.silva@ritain.io>
2023-05-10 13:51:18 +0100

    teste

--- ECR_ECS/INFRA/main.tf
+++ ECR_ECS/INFRA/main.tf
@@ -1,4 +1,4 @@
-
+/*
 terraform {
   required_providers {
     docker = {
@@ -11,9 +11,34 @@ terraform {
     }
   }
 }
+*/
+
+terraform {
+  required_version = ">= 1.0.7"
+  required_providers {
+    
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.20.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 1.0.0"
+    }
+  }
+  backend "s3" {
+    bucket = "silvaoscarda"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
 
 provider "docker" {}
 
+/*
 provider "aws" {
   region     = "us-east-1"
 }
+*/
