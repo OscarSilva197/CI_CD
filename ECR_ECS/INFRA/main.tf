509c95f5eccdf7fa8a8e748494e0255ba8e99964
oscardasilva <oscar.silva@ritain.io>
2023-05-11 09:57:41 +0100

    tes

--- ECR_ECS/INFRA/main.tf
+++ ECR_ECS/INFRA/main.tf
@@ -25,15 +25,13 @@ terraform {
       version = ">= 1.0.0"
     }
   }
-  }
-
 
   backend "s3" {
     bucket = "silvaoscarda"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
-
+}
 provider "aws" {
   access_key = "AKIA3GVPGHW4VN46WPWN"
   secret_key = "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"
