4e0f3d31eed1e4b9ced7ff607af482ae21c84ad2
oscardasilva <oscar.silva@ritain.io>
2023-05-11 11:24:48 +0100

    tes

--- ECR_ECS/INFRA/variables.tf
+++ ECR_ECS/INFRA/variables.tf
@@ -32,8 +32,9 @@ variable "private_cidr_b" {
   type        = string
   default     = "10.0.2.0/24"
 }
+/*
 variable "environment" {
   description = "Deployment Environment"
 }
-
+*/
 variable "docker_image_name" {}
