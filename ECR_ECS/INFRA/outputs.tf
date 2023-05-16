a5736d83ddec88bcc7e1e7b2f62ef926fa73865c
oscardasilva <oscar.silva@ritain.io>
2023-05-17 00:35:43 +0100

    test

--- ECR_ECS/INFRA/outputs.tf
+++ ECR_ECS/INFRA/outputs.tf
@@ -10,9 +10,8 @@ output "aws_ecs_cluster_capacity_providers" {
   value       = aws_ecs_cluster_capacity_providers.cluster.capacity_providers
   description = "Compute serverless engine for ECS"
 }
-
+*/
 output "alb_address" {
-  value = aws_alb.application_load_balancer.dns_name
+  value = "http://${aws_alb.application_load_balancer.dns_name}"
 }
 
-*/
